0e7934ca5f6ed0fe88155fc8c4a45f1b0eb0bb6c
root <danielkay62@gmail.com>
2023-10-12 16:46:02 -0700

    a SQL script that creates a table users with requirements

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -6,7 +6,7 @@
 -- If the table already exists, your script should not fail
 -- Your script can be executed on any database
 
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
